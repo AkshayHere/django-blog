f373d0d6b89425c373c13f00e348d77daea714e1
Dude <akshymohn@gmail.com>
2021-03-19 17:57:45 +0800

    Added Routes Updated .gitignore

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
 
 # Create your views here.
+def home(request: HttpRequest):
+    print(request)
+    return HttpResponse('<h1>Dude Home</h1>')
+
+def aboutUs(request):
+    return HttpResponse('<h3>About Dude</h3>')
