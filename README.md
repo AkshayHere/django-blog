929c67750b8b71547db82225740f00b7602cf97f
Dude <akshymohn@gmail.com>
2021-03-30 09:14:03 +0800

    Added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+## DJANGO KICKSTART
+
+Sample Blog Project based on Django
+
+[View Playlist](https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p)
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
 
+posts = [
+    {
+        'author' : 'The Dude',
+        'title' : 'Post One',
+        'content' : 'I am the dude man.',
+        'date_posted' : 'August 27. 2021',
+    },
+    {
+        'author' : 'Walter Sobchek',
+        'title' : 'Post Two',
+        'content' : 'Mark it Zero !!!',
+        'date_posted' : 'August 27. 2021',
+    },
+]
+
 # Create your views here.
 def home(request: HttpRequest):
-    print(request)
-    return HttpResponse('<h1>Dude Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def aboutUs(request):
     return HttpResponse('<h3>About Dude</h3>')
